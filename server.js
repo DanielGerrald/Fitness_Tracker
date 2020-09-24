4d3d00f9c75e9aa711d7ad3bb2f6f26c83f9041b
Daniel Gerrald <daniel.gerrald@icloud.com>
2020-09-23 23:22:23 -0400

    deploying

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/apiRoutes.js"));
